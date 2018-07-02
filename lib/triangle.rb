49e8842163679bc799804c622601f8c726cd4d7d
Leslie Stevens <flylady@mail.utexas.edu>
2018-07-02 20:47:50 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,10 @@
 class Triangle
+  attr_accessor :side_a, :side_b, :side_c
+
+  def initialize(side_a, side_b, side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+  end
   # write code here
 end
