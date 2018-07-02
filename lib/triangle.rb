510d6b3ac449345bcfc5d87a73fef39855efc72d
Leslie Stevens <flylady@mail.utexas.edu>
2018-07-02 20:49:03 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,10 +1,14 @@
 class Triangle
-  attr_accessor :side_a, :side_b, :side_c
+  attr_accessor :side_a, :side_b, :side_c, :type
 
   def initialize(side_a, side_b, side_c)
     @side_a = side_a
     @side_b = side_b
     @side_c = side_c
+  end
+
+  def kind
+    @types
   end
   # write code here
 end
